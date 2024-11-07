57b66332e8689db638715f51861f3b7d837e70a4
storngfornt <au0038173@gmail.com>
2024-11-07 13:03:12 +0600

    added blog in readme

--- assignment1/src/problem-3.ts
+++ assignment1/src/problem-3.ts
@@ -1,13 +1,13 @@
 {
   //function countWordOccurrences that takes a sentence (string) and a word (string). It will return the number of times the word appears in the sentence, case-insensitively.
 
-  type countWordOccurrences = (sentence: string, word: string) => any;
+  type countWordOccurrences = (sentence: string, word: string) => number;
 
   const countWordOccurrences: countWordOccurrences = (sentence, word) => {
     const lowerCaseSentence: string = sentence.toLowerCase();
     const lowerCaseWord: string = word.toLowerCase();
 
-    const cleanedSentence = lowerCaseSentence.replace(/[^a-z\s]/g, "");
+    const cleanedSentence:string = lowerCaseSentence.replace(/[^a-z\s]/g, "");
 
     const words: string[] = cleanedSentence.split(" ");
 
